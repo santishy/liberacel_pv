d5b1e625b3d20293229a16d1ca75cf82e75a205a
santishy <santi_shy@hotmail.com>
2022-02-08 19:13:51 -0600

    ...

--- app/Http/Controllers/UserCommissionsController.php
+++ app/Http/Controllers/UserCommissionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Commission;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -11,10 +12,15 @@ class UserCommissionsController extends Controller
     public function index()
     {
         if (request()->wantsJson()) {
-            if(request()->has('user_id')){
+            if (request()->has('user_id')) {
                 $user = User::find(request('user_id'));
-                $commissions  = $user->with('fastSales.commission');
-                return $commissions;
+                
+                $commissions  = Commission::with(['fastSales' => function($query){
+                    $query->where('user_id',request('user_id'));
+                }  ])->paginate();
+                return response()->json([
+                    'commissions' => $commissions
+                ]);
             }
         }
         $users = User::all();
--- app/Models/Commission.php
+++ app/Models/Commission.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,20 @@ class Commission extends Model
 {
     use HasFactory;
     protected $fillable = ['amount','fast_sale_id'];
+
+    public function fastSales(){
+        return $this->belongsTo(FastSale::class);
+    }
+    public function scopeBySales($query){
+        return $query->with(['fastSales' => function($sql) {
+            $sql->where('user_id',request('user_id'));
+        }]);
+    }
+    public function scopeWeek(Builder $query, $value)
+    {
+        $query->whereBetween('created_at', [
+            Carbon::now()->startOfWeek(),
+            Carbon::now()->endOfWeek()
+        ]);
+    }
 }
