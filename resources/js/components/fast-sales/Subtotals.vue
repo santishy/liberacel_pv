da34ac8b28e9b0076e26eb299c9480452637f59d
unknown <santi_shy@hotmail.com>
2022-06-08 16:34:49 -0500

    subtotals

---
+++ resources/js/components/fast-sales/Subtotals.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <span class="rounded-sm px-2 bg-green-700 text-white font-semibold">
+                Pts: {{ customerBonus.accumulated_points }}
+            </span>
+
+            <div class="text-sm px-2 ml-2">
+                Dinero electroinco: <span class="text-base font-semibold text-blue-700">$50</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
