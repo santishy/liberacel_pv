0d84bdaed15fea83a1ee664072db7b8b3a419961
Santiago <santi_shy@hotmail.com>
2024-05-08 19:13:11 -0600

    status component vue

--- resources/js/components/reports/ReportBy.vue
+++ resources/js/components/reports/ReportBy.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="flex flex-wrap flex-col items-start justify-start">
-        <toggle-component v-if="!isInGeneralReport" class="col-span-1 mr-2 mb-4"></toggle-component>
-        <div class="flex justify-center flex-wrap items-center w-full">
-            <div class="w-full flex flex-wrap justify-center gap-4  ">
-                <div class="p-1 border bg-orange-100 rounded text-slate-700 font-mono px-3">
+        <!-- <toggle-component v-if="!isInGeneralReport" class="col-span-1 mr-2 mb-4"></toggle-component> -->
+        <div class="flex justify-start flex-wrap items-center w-full mt-4">
+            <states-component v-if="!isInGeneralReport" />
+            <div class="w-full flex flex-wrap justify-start gap-4 mt-4 ">
+                <div class="p-1 border bg-orange-100 flex-grow rounded text-slate-700 font-mono px-3">
                     <span>Reporte por rango</span>
                     <date-picker @change="getReport(range, 'betweenDates')"
                         placeholder="Da click para eligir el rango de fechas" :lang="lang" value-type="YYYY-MM-DD"
@@ -30,15 +31,20 @@ import { mapState } from "vuex";
 import DatePicker from "vue2-datepicker";
 import "vue2-datepicker/index.css";
 import ConfigDatePicker from "../../mixins/ConfigDatePicker.js";
-import ToggleComponent from "./ToggleComponent.vue";
+//import ToggleComponent from "./ToggleComponent.vue";
+import StatesComponent from "./partials/StatesComponent.vue";
 export default {
     components: {
-        ToggleComponent,
+        //    ToggleComponent,
+        StatesComponent,
         DatePicker
     },
     mixins: [ConfigDatePicker],
     created() {
         EventBus.$on("status-filter-cancelled", status => {
+            this.status["filter[status]"] = status;
+        });
+        EventBus.$on("changed-status", status => {
             this.status["filter[status]"] = status;
         });
     },
---
+++ resources/js/components/reports/partials/StatesComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="flex flex-wrap gap-4">
+        <div>
+            <input type="radio" @change="handleStatusChange" value="completed" :checked="true" id="completed_status"
+                v-model="selectedOption" name="filter" class="hidden peer">
+            <label for="completed_status"
+                class="flex gap-2 items-center justify-around w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:bg-slate-700 peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                <div class="block">
+                    <div class="w-full text-sm leading-tight"><span class="font-bold">Status:</span> Completado</div>
+                </div>
+                <svg class="w-6 h-6 text-emerald-500" data-slot="icon" fill="none" stroke-width="1.5"
+                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                </svg>
+            </label>
+        </div>
+        <div>
+            <input type="radio" @change="handleStatusChange" value="pending" v-model="selectedOption"
+                id="pending_status" name="filter" class="hidden peer">
+            <label for="pending_status"
+                class="flex items-center justify-around w-full p-2 gap-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:bg-slate-700 peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                <div class="block">
+                    <div class="w-full text-sm leading-tight"><span class="font-bold">Status:</span> Pendiente</div>
+                </div>
+                <svg data-slot="icon" class="w-6 h-6  text-yellow-500" fill="none" stroke-width="1.5"
+                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z">
+                    </path>
+                </svg>
+            </label>
+        </div>
+        <div>
+            <input type="radio" @change="handleStatusChange" value="cancelled" id="cancelled_status"
+                v-model="selectedOption" name="filter" class="hidden peer">
+            <label for="cancelled_status"
+                class="flex gap-2 items-center justify-around w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:bg-slate-700 peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 ">
+                <div class="block">
+                    <div class="w-full text-sm leading-tight"><span class="font-bold">Status:</span> Cancelado</div>
+                </div>
+                <svg data-slot="icon" class="w-6 h-6 text-red-500" fill="none" stroke-width="1.5" stroke="currentColor"
+                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
+                </svg>
+            </label>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selectedOption: 'completed'
+        }
+    },
+    methods: {
+        handleStatusChange() {
+            EventBus.$emit('changed-status', this.selectedOption)
+        }
+    }
+}
+</script>
