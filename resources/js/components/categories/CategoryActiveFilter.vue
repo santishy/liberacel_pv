bd6eb335e65fef9f14d91f03587f1e8780ebe3fa
unknown <santi_shy@hotmail.com>
2024-06-06 16:16:50 -0600

    filter category

---
+++ resources/js/components/categories/CategoryActiveFilter.vue
@@ -0,0 +1,22 @@
+<template>
+    <form @submit.prevent="submit">
+        <input type="hidden" v-model="active"/>
+        <button>{{ getButtonText }}</button>
+    </form>
+</template>
+
+<script>
+export default {
+data(){
+    return {
+        active:false,
+    }
+},
+
+computed:{
+    getButtonText(){
+        return this.active ? "Categorías Activas" : "Categorías Desactivadas";
+    }
+}
+}
+</script>
--- resources/js/components/categories/CategoryFilter.vue
+++ resources/js/components/categories/CategoryFilter.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="p-4 bg-white rounded">
         <search-category />
+        <category-active-filter />
     </div>
 </template>
 
 <script>
 import SearchCategory from "./SearchCategory.vue";
+import CategoryActiveFilter from "./CategoryActiveFilter.vue";
 export default {
     components: {
         SearchCategory
